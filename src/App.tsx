98088a4fe6edfa6fec6d0572ff2a295679b74a90
GartiaLoria <loriamgartia@gmail.com>
2025-11-10 00:39:58 +0530

    removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import KeralaCountdown from './Component/KeralaCountdown';
 
 function App() {
